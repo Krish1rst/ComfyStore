d6cea4e4b1af8aa2efdf0415cbd5fea0d0cf41d1
Krishna Yadav <128611710+Krish1rst@users.noreply.github.com>
2024-04-17 23:58:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// global imports
+
 import './src/toggleSidebar.js';
 import './src/cart/toggleCart.js';
 import './src/cart/setupCart.js';
-// specific imports
+
 import fetchProducts from './src/fetchProducts.js';
 import { setupStore, store } from './src/store.js';
 import display from './src/displayProducts.js';
